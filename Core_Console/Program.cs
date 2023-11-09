1e36248140efb461e5b5b83378659d3f82f19d69
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-09 13:59:39 +0100

    upd

--- Core_Console/Program.cs
+++ Core_Console/Program.cs
@@ -6,8 +6,14 @@ int t2 = 2;
 Console.WriteLine(t2);
 GetSum();
 
-void GetSum()
+
+void GetSum(string s, int t)
 {
 	int sum = t + t2;
 	Console.WriteLine(sum);
+	int t3 = 3;
+	Console.WriteLine(t3);
+	void Test()
+	{
+	}		
 }
