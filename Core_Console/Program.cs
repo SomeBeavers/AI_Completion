94c60ec2491812cbec9694440ea66598b8771775
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-09 12:25:35 +0100

    Add code to Program

--- Core_Console/Program.cs
+++ Core_Console/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int t = 1;
+Console.WriteLine(t);
+int t2 = 2;
+Console.WriteLine(t2);
+GetSum();
+
+void GetSum()
+{
+	int sum = t + t2;
+	Console.WriteLine(sum);
+}
