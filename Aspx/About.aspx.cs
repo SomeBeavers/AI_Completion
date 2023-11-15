f0b7d2a2cc0fde61fca6d79f255ad40e931cb13f
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-15 11:23:28 +0100

    Smal code update.

--- Aspx/About.aspx.cs
+++ Aspx/About.aspx.cs
@@ -24,6 +24,10 @@ namespace Aspx
             // Display a message box with the value of the Name property
             MessageBox.Show(Name);
             
+            var dialog = new NameDialog();
+            dialog.ShowDialog();
+            Name = dialog.Name;
+            
             //TODO: show a dialog with ability to update Name
             
         }
@@ -34,4 +38,17 @@ namespace Aspx
         //    return Name;
         //}
     }
+
+    public class NameDialog
+    {
+        public string Name { get; set; }
+
+        public void ShowDialog()
+        {
+            // Here you can add code to display the dialog to the user and get the name.
+            // This is just a placeholder as the actual implementation will depend on the UI framework you are using.
+        }
+    }
+    
+    
 }
