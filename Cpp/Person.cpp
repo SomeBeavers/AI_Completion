9079ef5e3d52f0199672de8d529860662add4c86
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-15 13:40:31 +0100

    The most important changes include the addition of a new project for C++.

---
+++ Cpp/Person.cpp
@@ -0,0 +1,7 @@
+﻿#include "Person.h"
+
+void Person::Test()
+{
+	int t;
+	t = 1;
+}
--- Framework_Console_Old/Person.cs
+++ Framework_Console_Old/Person.cs
@@ -5,22 +5,7 @@
 		public string Name { get; set; }
 		public int Age { get; set; }
 		public int Birth { get; set; }
-		public Person(string name, int age)
-	    {
-	        Name = name;
-	        Age = age;
-	    }
-#if NETCOREAPP
-        public bool IsItCore() => true;
-#endif
-
-		public Person()
-		{
-			Name = "John";
-			Age = 30;
-		}
-
-		public override string ToString()
+        public override string ToString()
 		{
 			return $"Name: {Name}, Age: {Age}";
 		}
